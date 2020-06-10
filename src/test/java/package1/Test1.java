2260b3b636b57e0f40fb550030f6d985e0a35cec
anubhavsood92 <anubhav.sood92@gmail.com>
2020-06-11 01:43:20 +0530

    second commit in develop branch

--- src/test/java/package1/Test1.java
+++ src/test/java/package1/Test1.java
@@ -11,19 +11,6 @@ public class Test1
 {
 
 		
-	@Test(invocationCount = 5, threadPoolSize =  5)
-	public void m1()
-	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver =  new ChromeDriver();
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
-		System.out.println("m1 method");
-		System.out.println("m2 method");
-		System.out.println("m3 method");
-		driver.quit();
-		
-	}
 	
 		
 
