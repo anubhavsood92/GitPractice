b22c975b3dfe0a66b28fbe733045e8075dd9c7a1
anubhavsood92 <anubhav.sood92@gmail.com>
2020-06-11 01:49:43 +0530

    comitss

--- src/test/java/package1/Test1.java
+++ src/test/java/package1/Test1.java
@@ -11,6 +11,19 @@ public class Test1
 {
 
 		
+	@Test(invocationCount = 5, threadPoolSize =  5)
+	public void m1()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =  new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println("m1 method");
+		System.out.println("m2 method");
+	
+		driver.quit();
+		
+	}
 	
 		
 
